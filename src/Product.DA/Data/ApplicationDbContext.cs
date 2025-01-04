615eb12537efefdb785c9525ce7aaa7507d2f389
Станислав Саакян <stanis2253@gmail.com>
2025-01-04 12:19:50 +0300

    add model db

---
+++ src/Product.DA/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.DA.Models;
+
+namespace Product.DA.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product.DA.Models.Product> Product { get; set; }
+        public DbSet<Product.DA.Models.Category> Category { get; set; }
+    }
+}
---
+++ src/Product.DA/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.DA.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
